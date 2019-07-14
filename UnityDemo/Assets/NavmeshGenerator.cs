5f0d7492a5364eb94337ecb1f08191fdef5ff85b
Realmpact <vrenshrrgn44@hotmail.com>
2019-07-14 14:09:46 +0800

    HEHEHEH

---
+++ UnityDemo/Assets/NavmeshGenerator.cs
@@ -0,0 +1,346 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public enum NormalType
+{
+    NONE,
+    POSITIVE_Y,
+    NEGATIVE_Y,
+    POSITIVE_X,
+    NEGATIVE_X,
+    POSITVE_Z,
+    NEGATIVE_Z,
+    CUSTOM_AXIS
+}
+
+public struct Vertex
+{
+    public int id;
+    public Vector3 position;
+    public Vector3 normal;
+    public Dictionary<int, Vertex> neighbours;
+
+    public Vertex(int i, Vector3 pos, Vector3 nrm)
+    {
+        id = i;
+        position = pos;
+        normal = nrm;
+        neighbours = new Dictionary<int, Vertex>();
+    }
+
+    public bool IsSame(Vertex v)
+    {
+        return id == v.id; 
+    }
+}
+
+public struct Triangle
+{
+    public Vertex v0;
+    public Vertex v1;
+    public Vertex v2;
+    public Vector3 normal;
+
+    public Triangle(Vertex a, Vertex b, Vertex c)
+    {
+        v0 = a;
+        v1 = b;
+        v2 = c;
+        normal = ((v0.normal + v1.normal + v2.normal) / 3).normalized;
+    }
+
+    public bool IsAdjacent(Triangle triangle)
+    {
+        return (v0.IsSame(triangle.v0) || v0.IsSame(triangle.v1) || v0.IsSame(triangle.v2) ||
+            v1.IsSame(triangle.v0) || v1.IsSame(triangle.v1) || v1.IsSame(triangle.v2) ||
+            v2.IsSame(triangle.v0) || v2.IsSame(triangle.v1) || v2.IsSame(triangle.v2));
+    }
+
+    public bool IsConnected(Vertex v)
+    {
+        return v0.IsSame(v) || v1.IsSame(v) || v2.IsSame(v);
+    }
+}
+
+public struct Polygon
+{
+    public List<Vertex> vertices;
+    public Vector3 normal;
+
+    public Polygon(Triangle triangle)
+    {
+        vertices = new List<Vertex>();
+        vertices.Add(triangle.v0);
+        vertices.Add(triangle.v1);
+        vertices.Add(triangle.v2);
+        normal = triangle.normal;
+    }
+
+    public bool IsConnected(Vertex v)
+    {
+        for (int i = 0; i < vertices.Count; ++i)
+        {
+            if (vertices[i].IsSame(v))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public bool IsConnected(Triangle triangle)
+    {
+        for (int i = 0; i < vertices.Count; ++i)
+        {
+            if (triangle.IsConnected(vertices[i]))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public bool IsConnected(Polygon polygon)
+    {
+        for (int i = 0; i < vertices.Count; ++i)
+        {
+            if (polygon.IsConnected(vertices[i]))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public void AddTriangle(Triangle triangle)
+    {
+        if (!IsConnected(triangle.v0)) vertices.Add(triangle.v0);
+        if (!IsConnected(triangle.v1)) vertices.Add(triangle.v1);
+        if (!IsConnected(triangle.v2)) vertices.Add(triangle.v2);
+    }
+
+    public void AddPolygon(Polygon polygon)
+    {
+        for (int i = 0; i < polygon.vertices.Count; ++i)
+        {
+            if (!IsConnected(polygon.vertices[i])) vertices.Add(polygon.vertices[i]);
+        }
+    }
+
+    public void RemoveUnnecessaryVertices(int ignore, ref List<Polygon> polygons)
+    {
+        for (int i = vertices.Count - 1; i >= 0; --i)
+        {
+            bool toRemove = true;
+            for (int j = 0; j < polygons.Count; ++j)
+            {
+                if (j == ignore) continue;
+
+                if (polygons[j].IsConnected(vertices[i]))
+                {
+                    toRemove = false;
+                    break;
+                }
+            }
+
+            if (toRemove) vertices.RemoveAt(i);
+        }
+    }
+}
+
+public struct NavmeshTriangle
+{
+    public Vector3 v0;
+    public Vector3 v1;
+    public Vector3 v2;
+    public List<NavmeshTriangle> neighbours;
+}
+
+public class NavmeshGenerator:MonoBehaviour
+{
+    public MeshFilter meshFilter;
+
+    public NormalType typeNormal = NormalType.NONE;
+    //0-90
+    public float slopeAngle = 0f;
+    public float meshGap = 0f;
+    public float meshHeight = 0f;
+
+    public List<Triangle> allTriangles = new List<Triangle>();
+    public List<Polygon> allPolygons = new List<Polygon>();
+    public Dictionary<int, Vertex> uniqueVertices = new Dictionary<int, Vertex>();
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            ComputeNavmesh();
+        }
+
+        /*foreach (KeyValuePair<int, Vertex> vertex in uniqueVertices)
+        {
+            Color color = Random.ColorHSV();
+            Debug.DrawRay(vertex.Value.position, vertex.Value.normal, color);
+            foreach (KeyValuePair<int, Vertex> neighbour in vertex.Value.neighbours)
+            {
+                Debug.DrawLine(vertex.Value.position, neighbour.Value.position, color);
+            }
+        }*/
+
+        foreach (Polygon polygon in allPolygons)
+        {
+            foreach (Vertex vertex in polygon.vertices)
+            {
+                Debug.DrawRay(vertex.position, vertex.normal, Color.black);
+
+                foreach (KeyValuePair<int, Vertex> neighbour in uniqueVertices[vertex.id].neighbours)
+                {
+                    Debug.DrawLine(vertex.position, neighbour.Value.position, Color.red);
+                }
+            }
+        }
+    }
+
+    public void ComputeNavmesh()
+    {
+        if (!meshFilter) return;
+
+        Mesh mesh = meshFilter.mesh;
+        allTriangles.Clear();
+        allPolygons.Clear();
+        uniqueVertices.Clear();
+
+        float cosSlope = Mathf.Cos(slopeAngle * Mathf.Deg2Rad);
+        for (int i = 0; i < mesh.triangles.Length; i += 3)
+        {
+            int idxTri0 = mesh.triangles[i];
+            int idxTri1 = mesh.triangles[i + 1];
+            int idxTri2 = mesh.triangles[i + 2];
+
+            Vertex v0 = new Vertex(idxTri0, mesh.vertices[idxTri0], mesh.normals[idxTri0]);
+            Vertex v1 = new Vertex(idxTri1, mesh.vertices[idxTri1], mesh.normals[idxTri1]);
+            Vertex v2 = new Vertex(idxTri2, mesh.vertices[idxTri2], mesh.normals[idxTri2]);
+            Triangle triangle = new Triangle(v0, v1, v2);
+            if (Vector3.Dot(Vector3.up, triangle.normal) >= cosSlope)
+            {
+                allTriangles.Add(new Triangle(v0, v1, v2));
+
+                if (!uniqueVertices.ContainsKey(idxTri0)) uniqueVertices.Add(idxTri0, v0);
+                if (!uniqueVertices.ContainsKey(idxTri1)) uniqueVertices.Add(idxTri1, v1);
+                if (!uniqueVertices.ContainsKey(idxTri2)) uniqueVertices.Add(idxTri2, v2);
+
+                if (!uniqueVertices[idxTri0].neighbours.ContainsKey(idxTri1)) uniqueVertices[idxTri0].neighbours.Add(idxTri1, v1);
+                if (!uniqueVertices[idxTri0].neighbours.ContainsKey(idxTri2)) uniqueVertices[idxTri0].neighbours.Add(idxTri2, v2);
+                if (!uniqueVertices[idxTri1].neighbours.ContainsKey(idxTri0)) uniqueVertices[idxTri1].neighbours.Add(idxTri0, v0);
+                if (!uniqueVertices[idxTri1].neighbours.ContainsKey(idxTri2)) uniqueVertices[idxTri1].neighbours.Add(idxTri2, v2);
+                if (!uniqueVertices[idxTri2].neighbours.ContainsKey(idxTri1)) uniqueVertices[idxTri2].neighbours.Add(idxTri1, v1);
+                if (!uniqueVertices[idxTri2].neighbours.ContainsKey(idxTri0)) uniqueVertices[idxTri2].neighbours.Add(idxTri0, v0);
+            }
+        }
+
+        for (int i = 0; i < allTriangles.Count; ++i)
+        {
+            Triangle triangle = allTriangles[i];
+            Polygon polygon = new Polygon(triangle);
+            for (int j = allTriangles.Count - 1; j > i; --j)
+            {
+                Triangle neighbour = allTriangles[j];
+                if (Vector3.Dot(polygon.normal, neighbour.normal) == 1.0f)
+                {
+                    if (polygon.IsConnected(neighbour))
+                    {
+                        polygon.AddTriangle(neighbour);
+                        allTriangles.RemoveAt(j);
+                    }
+                }
+
+            }
+            allPolygons.Add(polygon);
+        }
+
+        bool change = false;
+        do
+        {
+            change = false;
+            for (int i = 0; i < allPolygons.Count; ++i)
+            {
+                Polygon polygon = allPolygons[i];
+                for (int j = allPolygons.Count - 1; j > i; --j)
+                {
+                    Polygon neighbour = allPolygons[j];
+                    if (Vector3.Dot(polygon.normal, neighbour.normal) == 1.0f)
+                    {
+                        if (polygon.IsConnected(neighbour))
+                        {
+                            polygon.AddPolygon(neighbour);
+                            allPolygons.RemoveAt(j);
+                            change = true;
+                        }
+                    }
+                }
+            }
+        }
+        while (change);
+
+        List<int> idsRemove = new List<int>();
+        foreach (KeyValuePair<int, Vertex> vertex in uniqueVertices)
+        {
+            int multipier = 0;
+            foreach (Polygon polygon in allPolygons)
+            {
+                if (polygon.IsConnected(vertex.Value))
+                {
+                    ++multipier;
+                }
+            }
+            if (vertex.Value.neighbours.Count > 3 * multipier)
+            {
+                idsRemove.Add(vertex.Key);
+            }
+        }
+        foreach (KeyValuePair<int, Vertex> vertex in uniqueVertices)
+        {
+            if (idsRemove.Contains(vertex.Key)) continue;
+
+            foreach (int id in idsRemove)
+            {
+                if (vertex.Value.neighbours.ContainsKey(id)) vertex.Value.neighbours.Remove(id);
+            }
+        }
+        foreach (int id in idsRemove)
+        {
+            uniqueVertices.Remove(id);
+        }
+
+        for (int i = allPolygons.Count - 1; i >= 0; --i)
+        {
+            for (int j = allPolygons[i].vertices.Count - 1; j >= 0; --j)
+            {
+                if (!uniqueVertices.ContainsKey(allPolygons[i].vertices[j].id))
+                {
+                    allPolygons[i].vertices.RemoveAt(j);
+                }
+            }
+
+            if (allPolygons[i].vertices.Count < 3) allPolygons.RemoveAt(i);
+            else
+            {
+                for (int j = 0; j < allPolygons[i].vertices.Count - 1; ++j)
+                {
+                    int id = allPolygons[i].vertices[j].id;
+                    for (int k = j + 1; k < allPolygons[i].vertices.Count; ++k)
+                    {
+                        int neighbourId = allPolygons[i].vertices[k].id;
+                        if (!uniqueVertices[id].neighbours.ContainsKey(neighbourId)) uniqueVertices[id].neighbours.Add(neighbourId, allPolygons[i].vertices[k]);
+                        if (!uniqueVertices[neighbourId].neighbours.ContainsKey(id)) uniqueVertices[neighbourId].neighbours.Add(id, allPolygons[i].vertices[j]);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ UnityDemo/Assets/VertexFinder.cs
@@ -0,0 +1,183 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavMeshVertex
+{
+    public NavMeshVertex()
+    {
+        neighbours = new List<NavMeshVertex>();
+        cost = new List<float>();
+        Reset();
+    }
+
+    public void Reset()
+    {
+        totalCost = Mathf.Infinity;
+        parent = null;
+        isVisited = false;
+    }
+
+    public Vector3 point;
+    public List<NavMeshVertex> neighbours;
+    public List<float> cost;
+    public float totalCost;
+    public NavMeshVertex parent;
+    public bool isVisited;
+}
+
+public class VertexFinder : MonoBehaviour
+{
+    List<NavMeshVertex> vertices;
+
+    // pre calculate path
+    // find neighbour
+    // profit
+
+    void Start()
+    {
+        vertices = new List<NavMeshVertex>();
+
+        NavMeshTriangulation nmt = NavMesh.CalculateTriangulation();
+        foreach (Vector3 vec in nmt.vertices)
+        {
+            bool isAdd = true;
+            foreach (var vtx in vertices)
+                if (vtx.point == vec)
+                {
+                    isAdd = false;
+                    break;
+                }
+
+            if (isAdd)
+            {
+                NavMeshVertex toAdd = new NavMeshVertex();
+                toAdd.point = vec;
+                vertices.Add(toAdd);
+            }
+        }
+
+        Debug.Log("Vertex count: " + vertices.Count);
+
+        foreach (NavMeshVertex vtx in vertices)
+        {
+            //vtx.neighbours = new List<NavMeshVertex>();
+            foreach (NavMeshVertex neighbour in vertices)
+            {
+                if (vtx.point == neighbour.point)
+                    continue;
+               
+                Vector3 dir = (neighbour.point - vtx.point);
+                float dist = Vector3.Distance(vtx.point, neighbour.point);
+                if (!Physics.Raycast(vtx.point, dir.normalized, dist))
+                {
+                    vtx.neighbours.Add(neighbour);
+                    vtx.cost.Add(dist);
+                }
+            }
+        }
+    }
+
+    int testIndexOne = 0;
+    int testIndexTwo = 1;
+    void Update()
+    {
+        if (Application.isPlaying)
+        {
+            if (testIndexOne < vertices.Count - 1 && Input.GetKeyDown(KeyCode.A))
+                ++testIndexOne;
+            else if (testIndexOne >= 0 && Input.GetKeyDown(KeyCode.S))
+                --testIndexOne;
+            
+            if (testIndexTwo < vertices.Count && Input.GetKeyDown(KeyCode.K))
+                ++testIndexTwo;
+            else if (testIndexTwo >= 1 && Input.GetKeyDown(KeyCode.L))
+                --testIndexTwo;
+        }
+    }
+
+
+    private void OnDrawGizmos()
+    {
+        if (vertices == null || vertices.Count == 0)
+            return;
+
+        Gizmos.color = Color.red;
+        List<Vector3> path = new List<Vector3>();
+        if (GetPath(vertices[testIndexOne], vertices[vertices.Count - testIndexTwo], out path))
+        {
+            Debug.Log("path found");
+            for (int i = 0; i < path.Count - 1; ++i)
+            {
+                Gizmos.DrawLine(path[i], path[i + 1]);
+                Gizmos.DrawSphere(path[i + 1], 0.1f);
+            }
+        }
+    }
+
+    public bool GetPath(NavMeshVertex origin, NavMeshVertex goal, out List<Vector3> path)
+    {
+        path = new List<Vector3>();
+
+        List<NavMeshVertex> openList = new List<NavMeshVertex>();
+
+        origin.totalCost = 0.0f;
+        openList.Add(origin);
+
+        while (openList.Count > 0)
+        {
+            // Find cheapest node
+            int cheapestIndex = 0;
+            for (int i = 1; i < openList.Count; ++i)
+            {
+                if (openList[i].totalCost < openList[cheapestIndex].totalCost)
+                    cheapestIndex = i;
+            }
+            var currPoint = openList[cheapestIndex];
+            openList.RemoveAt(cheapestIndex);
+
+            // Reached
+            if (currPoint == goal)
+            {
+                while (currPoint != origin)
+                {
+                    path.Insert(0, currPoint.point);
+                    currPoint = currPoint.parent;
+                }
+                path.Insert(0, origin.point);
+                Debug.Log(path.Count);
+
+                foreach (var vtx in vertices)
+                    vtx.Reset();
+
+                return true;
+            }
+
+            // Loop neighbours
+            for (int i = 0; i < currPoint.neighbours.Count; ++i)
+            {
+                // Skip visited vertex
+                if (currPoint.neighbours[i].isVisited)
+                    continue;
+
+                float newCost = currPoint.totalCost + currPoint.cost[i];
+                if (newCost < currPoint.neighbours[i].totalCost)
+                {
+                    currPoint.neighbours[i].totalCost = newCost;
+                    currPoint.neighbours[i].parent = currPoint;
+                }
+
+                if (!openList.Contains(currPoint.neighbours[i]))
+                    openList.Add(currPoint.neighbours[i]);
+            }
+
+            currPoint.isVisited = true;
+        }
+
+        foreach (var vtx in vertices)
+            vtx.Reset();
+
+        return false;
+    }
+}
